700e501d299de06508e6d3761ecd101495c7f9f3
LucasGabriell97 <lucas.gabriell97@hotmail.com>
2021-11-08 11:36:51 -0300

    Material-ui library installation and deleting some files that will not be used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core';
 
-function App() {
+export default function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3f51b5',
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <div>
 
-export default App;
+      </div>
+    </ThemeProvider>
+  );
+};
