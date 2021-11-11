5ab05df542692ebbe682a1fe3ef423595326e0a9
LucasGabriell97 <lucas.gabriell97@hotmail.com>
2021-11-11 11:36:42 -0300

    Addition of youtube logo, other buttons and side menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { Box, AppBar, Toolbar, IconButton, Button, ListItemText, ListItemIcon, InboxIcon, MailIcon, Divider, List, ListItem, Typography, CssBaseline, Drawer, } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AppsIcon from '@material-ui/icons/Apps';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+
+import HomeIcon from '@material-ui/icons/Home';
+import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+
+import VideoLibraryOutlinedIcon from '@material-ui/icons/VideoLibraryOutlined';
+import History from '@material-ui/icons/History';
+
+import AddCircle from '@material-ui/icons/AddCircle';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -10,12 +23,36 @@ const useStyles = makeStyles((theme) => ({
     },
     appBar: {
         boxShadow: 'none',
+        zIndex: theme.zIndex.drawer + 1,
     },
     icons: {
-        paddingRight: theme.spacing(5),
+        //paddingRight: theme.spacing(3),
+        color: '#000'
     },
+    logo: {
+        height: 30,
+        paddingLeft: theme.spacing(1.5),
+    },
+    drawer: {
+        width: 240,
+        flexShrink: 0,
+      },
+      drawerPaper: {
+        width: 240,
+        borderRight: 'none',
+      },
     grow: {
         flexGrow: 1,
+    },
+    button: {
+        borderRadius: 2,
+    },
+    ListItemText: {
+        fontSize: 14,
+    },
+    listItem: {
+        paddingTop: 5,
+        paddingBottom: 5,
     },
   }));
   
@@ -30,27 +67,108 @@ function Home() {
                     <Toolbar>
                         <IconButton
                             edge='start'
-                            className={classes.icons}
                             color='inherit'
                             aria-label='menu'
-                            //sx={{ mr: 2 }}
+                            //sx={{ fontSize:  }}
                         >
                             <MenuIcon />
                         </IconButton>
+                        <img src='/imgs/yt_logo.png' alt='Logo' className={classes.logo}/>
                         <div className={classes.grow} />
                         <IconButton className={classes.icons} color='inherit'>
-                            <MenuIcon />
+                            <KeyboardVoiceIcon />
                         </IconButton>
                         <IconButton className={classes.icons} color='inherit'>
-                            <MenuIcon />
+                            <AppsIcon />
                         </IconButton>
                         <IconButton className={classes.icons} color='inherit'>
-                            <MenuIcon />
-                        </IconButton>
-                        <Button startIcon={<AccountCircleOutlinedIcon />} variant="outlined" color="secondary">Fazer Login</Button>
+                            <MoreVertIcon />
+                        </IconButton> 
+                        <Button startIcon={<AccountCircleOutlinedIcon />} className={classes.button} variant="outlined" color="secondary">Fazer Login</Button>
                     </Toolbar>
                 </AppBar>
-            </Box>    
+            </Box>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <Toolbar />
+                <div className={classes.drawerContainer}>
+                    <List>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon className={classes.icons} >{<HomeIcon />}</ListItemIcon>
+                            <ListItemText 
+                                classes={{
+                                    primary: classes.ListItemText, 
+                                }}
+                                primary={'Início'} 
+                            />
+                        </ListItem>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon className={classes.icons} >{<ExploreOutlinedIcon />}</ListItemIcon>
+                            <ListItemText 
+                                classes={{
+                                    primary: classes.ListItemText, 
+                                }}
+                                primary={'Explorar'} 
+                            />
+                        </ListItem>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon className={classes.icons} >{<SubscriptionsOutlinedIcon />}</ListItemIcon>
+                            <ListItemText 
+                                classes={{
+                                    primary: classes.ListItemText, 
+                                }}
+                                primary={'Inscrições'} 
+                            />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <List>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon className={classes.icons}>
+                                <VideoLibraryOutlinedIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                classes={{
+                                    primary: classes.ListItemText,
+                                }} 
+                                primary={'Biblioteca'} 
+                            />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon className={classes.icons}>
+                                <History />
+                            </ListItemIcon>
+                            <ListItemText 
+                                classes={{
+                                    primary: classes.ListItemText,
+                                }}
+                                primary={'Histórico'} 
+                            />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <Box p={4}>
+                        <Typography variant='body2'>
+                            Faça login para curtir vídeos, comentar e se inscrever.
+                        </Typography>  
+                        <Box mt={2}>
+                            <Button
+                                className={classes.button}
+                                variant='outlined'
+                                color='secondary'
+                                startIcon={<AccountCircleOutlinedIcon />}  
+                            >   
+                                Fazer Login
+                            </Button> 
+                        </Box>               
+                    </Box>
+                </div>
+            </Drawer>    
         </div>
     )
 }
